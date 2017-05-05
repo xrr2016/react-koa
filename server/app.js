80354524427d2f0666ff8362583465e53eee2f80
xrr2016 <xiaoranran1993@163.com>
2017-05-05 22:22:54 +0800

    :heavy_plus_sign:

--- server/app.js
+++ server/app.js
@@ -1,5 +1,5 @@
 import Koa from 'koa'
-
+import Sequelize from 'sequelize'
 import { port } from './config/config'
 
 const app = new Koa()
---
+++ server/model/anime.js
@@ -0,0 +1,7 @@
+import sequelize from 'sequelize'
+
+const Anime = sequelize.define('Anime', {
+  
+}) 
+
+export default Anime 
