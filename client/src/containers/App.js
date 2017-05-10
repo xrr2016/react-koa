1aae49c7fadbed4173b67eee3fb26473231380e7
xrr2016 <xiaoranran1993@163.com>
2017-05-10 22:59:00 +0800

    :zap:

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
-import Navbar from '../components/Navbar'
+import AppHeader from '../components/AppHeader'
+import { Layout } from 'antd'
 
 class App extends Component {
   render () {
     return (
-     <div>App!</div>
+      <div>
+        <Layout>
+          <AppHeader />
+        </Layout>
+      </div>
     )
   }
 }
