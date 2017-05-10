64cd8c0eb70b38d98d2d6a7b15711fd5758e7d89
xrr2016 <xiaoranran1993@163.com>
2017-05-10 23:29:31 +0800

    :bulb: app-header

--- client/src/components/AppHeader.js
+++ client/src/components/AppHeader.js
@@ -2,20 +2,40 @@ import React, { Component } from 'react'
 import { Layout, Menu } from 'antd'
 const { Header } = Layout
 
+const headerStyle = {
+  position: 'fixed',
+  left: 0,
+  top: 0,
+  width: '100%',
+  height: 60,
+  backgroundColor: '#fff',
+  boxShadow: '0px 1px 1px 0px rgba(0,0,102,0.1)'
+}
+const menuStyle = {
+  height: 60,
+  border: 'none'
+}
+const menuItemStyle = {
+  height: 60,
+  lineHeight: '60px',
+  fontSize: 16,
+  color: '#333'
+}
+
 class AppHeader extends Component {
   render () {
     return (
-      <Header>
+      <Header style={headerStyle}>
         <div className='logo' />
         <Menu
-          theme='dark'
-          mode='horizontal'
-          defaultSelectedKeys={['2']}
-          style={{ lineHeight: '64px' }}
-        >
-          <Menu.Item key='1'>nav 1</Menu.Item>
-          <Menu.Item key='2'>nav 2</Menu.Item>
-          <Menu.Item key='3'>nav 3</Menu.Item>
+          style={menuStyle}  
+          theme="white"
+          mode="horizontal"
+          defaultSelectedKeys={['1']}
+          >
+          <Menu.Item style={menuItemStyle} key="1">全部</Menu.Item>
+          <Menu.Item style={menuItemStyle} key="2">推荐</Menu.Item>
+          <Menu.Item style={menuItemStyle} key="3">收藏</Menu.Item>
         </Menu>
       </Header>
     )
--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -2,10 +2,14 @@ import React, { Component } from 'react'
 import AppHeader from '../components/AppHeader'
 import { Layout } from 'antd'
 
+const style = {
+  backgroundColor: '#f4f4f4'
+}
+
 class App extends Component {
   render () {
     return (
-      <div>
+      <div style={style}>
         <Layout>
           <AppHeader />
         </Layout>
